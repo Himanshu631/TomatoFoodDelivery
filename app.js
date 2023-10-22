bd7ee8a7e9bdbfd6d99d7c7e9625c9f092781969
Himanshu631 <himanshupauljsr@gmail.com>
2023-10-22 23:24:32 +0530

    updated navbar

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-conatiner">
+                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJgUs_-Phd7DpL_Q4QlLeoiFDCrngm6xMIog&usqp=CAU" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div> 
+        </div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
+
