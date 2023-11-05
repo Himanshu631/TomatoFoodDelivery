914d3cd3eb7811197a71eb433505936a3e4a751d
Himanshu631 <himanshupauljsr@gmail.com>
2023-11-05 16:35:10 +0530

    completed class based components

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,21 +3,41 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);
-        console.log(props);
+        //console.log(props);
 
-        this.state = {
+        this.state= {
+            userInfo:{
+            name: "Dummy",
+            location: "Default",
+            avatar_url: "myphoto.com",
+            }
+        }
+
+        {/*this.state = {
             count:0,
             count2:2
-        }
-        
+        }*/}    
     }
+
+    async componentDidMount(){
+        const data = await fetch("https://api.github.com/users/himanshu631");
+        const json = await data.json();
+
+        console.log(json);
+
+        this.setState({
+            userInfo: json,
+        });
+    }
+
+
     render() {
-        const { name,location,contact } = this.props;
-        const { count,count2 } = this.state;
+        const { name,location,login,avatar_url } = this.state.userInfo;
+        //const { count,count2 } = this.state;
         
         return (
             <div className="user-card">
-                <h2>Count : {count}</h2>
+                {/*<h2>Count : {count}</h2>
                 <button onClick={()=>{
                     this.setState({
                         count:this.state.count+1
@@ -32,9 +52,12 @@ class UserClass extends React.Component{
                 }}>
                     Reset Count 
                 </button>
+
+            */}
+                <img src= {avatar_url}/>
                 <h2>Name : {name}</h2>
                 <h3>Location: {location} </h3>
-                <h4>Contact: {contact} </h4>
+                <h4>Contact: {login} </h4>
             </div>  
         );
     }
